6bb749eba28bf00d28d2646a931bc30f6e112c81
stephangrundner <rukyg9Seph>
2014-11-25 22:12:29 +0100

    Development

--- grails-vaadin7-spring-security-demo/grails-app/conf/BuildConfig.groovy
+++ grails-vaadin7-spring-security-demo/grails-app/conf/BuildConfig.groovy
@@ -23,7 +23,7 @@ grails.project.fork = [
 
 grails.project.dependency.resolver = "maven" // or ivy
 
-//grails.plugin.location."vaadin-core" = "../grails-vaadin7-spring-security-plugin"
+grails.plugin.location."vaadin-spring-security" = "../grails-vaadin7-spring-security-plugin"
 
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
