34a69670f84034aa28bcad4f774561ccf54a5082
stephangrundner <rukyg8seph>
2014-11-27 17:21:55 +0100

    Extended Demo

--- grails-vaadin7-spring-security-demo/grails-app/conf/VaadinConfig.groovy
+++ grails-vaadin7-spring-security-demo/grails-app/conf/VaadinConfig.groovy
@@ -6,6 +6,15 @@ vaadin {
             ui = "default"
             theme = "valo"
             access = ["ROLE_ADMIN"]
+
+            fragments {
+
+                "index" {
+                    view = "index"
+                    access = ["ROLE_ADMIN"]
+                }
+
+            }
         }
 
     }
---
+++ grails-vaadin7-spring-security-demo/grails-app/vaadin/demo/IndexView.groovy
@@ -0,0 +1,18 @@
+package demo
+
+import com.vaadin.navigator.View
+import com.vaadin.navigator.ViewChangeListener
+import com.vaadin.ui.CustomComponent
+import com.vaadin.ui.Label
+
+class IndexView extends CustomComponent implements View {
+
+    IndexView() {
+
+    }
+
+    @Override
+    void enter(ViewChangeListener.ViewChangeEvent viewChangeEvent) {
+        compositionRoot = new Label("Index.")
+    }
+}
