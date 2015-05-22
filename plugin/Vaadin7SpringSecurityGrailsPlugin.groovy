233f0f9f407c3f6ffe3e6c2d7db6442814c6a199
stephangrundner <rukyg8seph>
2015-05-22 21:32:54 +0200

    Release v2.1

--- plugin/Vaadin7SpringSecurityGrailsPlugin.groovy
+++ plugin/Vaadin7SpringSecurityGrailsPlugin.groovy
@@ -14,7 +14,7 @@ import org.vaadin.grails.server.UriMappings
  */
 class Vaadin7SpringSecurityGrailsPlugin {
 
-    def version = "2.1-SNAPSHOT"
+    def version = "2.1"
     def grailsVersion = "2.4 > *"
 
     def group = "com.github.vaadin-for-grails"
